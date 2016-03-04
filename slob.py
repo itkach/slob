504f280ab8c5d5397d74928becdc2c8c97937977
Igor Tkach <itkach@aarddict.org>
2016-03-03 22:57:32 -0500

    Fix split arg handling

--- slob.py
+++ slob.py
@@ -1965,7 +1965,7 @@ def _cli_convert(args):
                        if args.compression is None
                        else args.compression)
         min_bin_size = 1024*args.min_bin_size
-        split = 1024*1024*args.split - 1
+        split = 1024*1024*args.split
 
         print('Mapping blobs to keys...')
         blob_to_refs = [collections.defaultdict(lambda: array.array('L'))
